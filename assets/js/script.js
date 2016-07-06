8ceb8b6d3c5ee0e7389abf54525a66f85af2119f
Kyle <kyle.wolff7@gmail.com>
2016-07-05 20:48:13 -0500

    add Jquery underline

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,4 +2,7 @@ $(document).ready(function() {
   $('#jBold').click(function() {
     document.execCommand('bold');
   });
+  $('#jUnderline').click(function() {
+    document.execCommand('underline');
+  });
 });
