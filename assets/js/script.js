a08df4aaff4b49374132480f31ed6bf41c547d71
Kyle <kyle.wolff7@gmail.com>
2016-07-09 20:11:05 -0500

    Remove change font

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,13 +8,4 @@ $(document).ready(function() {
    $('#jItalic').click(function() {
     document.execCommand('italic');
   });
-  function changeFont() {
-    document.execCommand("fontSize", false, "7");
-    var fontElements = document.getElementsByTagName("font");
-    for (var i = 0, len = fontElements.length; i < len; ++i) {
-        if (fontElements[i].size == "7") {
-            fontElements[i].removeAttribute("size");
-            fontElements[i].style.fontSize = "30px";
-        }
-    }
-}):
+});
